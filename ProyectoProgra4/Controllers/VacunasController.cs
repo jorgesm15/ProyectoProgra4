1712441c31d810f134937b84eec29e2d610c5796
Jorge Segnini <jorge.segninim@gmail.com>
2021-03-13 20:47:00 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoProgra4/Controllers/VacunasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgra4.Controllers
+{
+    public class VacunasController : Controller
+    {
+        // GET: Vacunas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
