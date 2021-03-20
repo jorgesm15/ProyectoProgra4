d1009ecd1b664f0a124e771979ad25b970f6972c
Ana Salas <ana.salasp29@gmail.com>
2021-03-19 19:32:27 -0600

    Pagina de Inicio. (aun me falta el diseño) Modificacion del navbar

---
+++ ProyectoProgra4/Controllers/PaginaPrincipalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgra4.Controllers
+{
+    public class PaginaPrincipalController : Controller
+    {
+        // GET: PaginaPrincipal
+        [HttpGet]
+        public ActionResult PaginaPrincipal()
+        {
+            return View();
+        }
+    }
+}
