75ee76c98b32dbd4a81b72807b80a981824e3e68
Ana Salas <ana.salasp29@gmail.com>
2021-03-22 22:16:44 -0600

    .

--- ProyectoProgra4/Controllers/PaginaPrincipalController.cs
+++ ProyectoProgra4/Controllers/PaginaPrincipalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static ProyectoProgra4.ActionFilter.NoDirectAccess;
 
 namespace ProyectoProgra4.Controllers
 {
