e8c59ce68204b31341f29b086d31282346b27f0e
SirGusx <54556687+SirGusx@users.noreply.github.com>
2021-04-07 16:49:30 -0600

    Formulario de contacto, en página principal

--- ProyectoProgra4/Controllers/PaginaPrincipalController.cs
+++ ProyectoProgra4/Controllers/PaginaPrincipalController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 
 namespace ProyectoProgra4.Controllers
@@ -12,5 +15,55 @@ namespace ProyectoProgra4.Controllers
             ViewBag.Message = TempData["Message"];
             return View();
         }
+
+        [HttpGet]
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string nombre, string apellidos, string telefono, string email, string mensaje)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("proyectoprogra4fidelitas@gmail.com", "Proyecto Gimnasio");
+                    var receiverEmail = new MailAddress("proyectoprogra4fidelitas@gmail.com", "Proyecto Gimnasio");
+                    var password = "A723r88pT57qh@f34tGh344%gd37";
+                    var sub = "Solicitud de contacto de la Web";
+                    var body = "Nombre: " + nombre + "\n\r" + "Apellidos: " + apellidos + "\n\r" + "Telefono: " + telefono + "\n\r" + "Email: " + email + "\n\r" + "Mensaje: " + mensaje;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = sub,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View("PaginaPrincipal");
+
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+
+
     }
+
+
 }
