1a81cc217b399a35273716ea9428fc125715c624
Jorge Segnini <jorge.segninim@gmail.com>
2021-04-09 17:34:23 -0600

    Registro validado

--- ProyectoProgra4/Entidades/clsUsuario.cs
+++ ProyectoProgra4/Entidades/clsUsuario.cs
@@ -8,22 +8,36 @@ namespace ProyectoProgra4.Entidades
 {
     public class clsUsuario
     {
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string cedula { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string nombre { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string primerApellido { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string segundoApellido { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string correo { get; set; }
-        [Required(ErrorMessage = "This field can not be empty.")]
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         [Range(0, 100, ErrorMessage = "Ingrese un numero entre 1-100")]
         public int edad { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string contrasenia { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string direccion { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string telefono { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string telefonoEmergencia { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public decimal peso { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public decimal estatura { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string condicionesMedicas { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public string tipoSangre { get; set; }
+        [Required(ErrorMessage = "Este campo no puede ser vacío.")]
         public int motivo{ get; set; }
 
 
