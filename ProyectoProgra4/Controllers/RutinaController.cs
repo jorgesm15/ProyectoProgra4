b5fdaece26bf4eade1d25b161b2261a67920b81a
PAULA GUERRERO <pguerrero50660@ufide.ac.cr>
2021-04-09 15:51:36 -0600

    Sección de rutinas completa

---
+++ ProyectoProgra4/Controllers/RutinaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoProgra4.Entidades;
+
+namespace ProyectoProgra4.Controllers
+{
+    public class RutinaController : Controller
+    {
+
+        [HttpPost]
+        public void VerRutina()
+        {
+            using (var contexto = new ProyectoEntities())
+            {
+                var respuesta = (from x in contexto.Motivo select x).ToList();
+                List<SelectListItem> listilla = new List<SelectListItem>();
+
+                foreach (var item in respuesta)
+                {
+                    listilla.Add(new SelectListItem { Value = item.ID_Motivo.ToString(), Text = item.Descripcion });
+                }
+
+
+                ViewBag.ListaComboDis = listilla;
+            }
+        }
+
+        public ActionResult Rutina()
+        {
+            VerRutina();
+
+            return View();
+        }
+    }
+}
