9f923675f4acd6f686a341f3b6b3ad6982134130
Jorge Segnini <jorge.segninim@gmail.com>
2021-04-26 12:43:09 -0600

    Validación en rutinas y modificaciones en administrar

--- ProyectoProgra4/Instructor.cs
+++ ProyectoProgra4/Instructor.cs
@@ -11,7 +11,7 @@ namespace ProyectoProgra4
 {
     using System;
     using System.Collections.Generic;
-
+    
     public partial class Instructor
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,7 +19,7 @@ namespace ProyectoProgra4
         {
             this.Disciplinas = new HashSet<Disciplinas>();
         }
-
+    
         public string ID_Instructor { get; set; }
         public string Nombre { get; set; }
         public string PrimerApellido { get; set; }
@@ -39,7 +39,6 @@ namespace ProyectoProgra4
 
         public string nombreDis { get; set; }
         public string nombreAdm { get; set; }
-
 
         public virtual Administrador Administrador { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
