de456846b935bfcca6c335ac069ea8da71b2bea7
Jorge Segnini <jorge.segninim@gmail.com>
2021-04-25 22:56:38 -0600

    Controlador de Instructores y rutinas

---
+++ ProyectoProgra4/Controllers/DashboardIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgra4.Controllers
+{
+    public class DashboardIController : Controller
+    {
+        // GET: DashboardI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
